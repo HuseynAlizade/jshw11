645bb3368c8d07a3c47c8e41a053da491d1bc98c
HuseynAlizade <huseynalizade2010@gmail.com>
2024-08-31 11:00:45 +0400

    jshw11

---
+++ script.js
@@ -0,0 +1,52 @@
+const addInput= document.getElementById('input'); 
+const addẞtn = document.getElementById('btn');
+const taskListContainer= document.querySelector(".taskList");
+const tasklist= document.querySelector('.taskList ul');
+
+let todos = []
+
+addẞtn.addEventListener('click', function(e){
+e.preventDefault();
+
+const taskText= addInput.value.trim();
+
+if (!taskText) return;
+
+todos.push(taskText);
+console.log(todos);
+addInput.value ='';
+
+sessionStorage.setItem("todos", JSON.stringify(todos));
+})
+
+//! Update Ul
+function updateTaskList() {
+tasklist.innerHTML="";
+
+taskListContainer.style.display=todos.length? "block": "none";
+
+todos.forEach(function (element, index) {
+const liel= document.createElement("li");
+const spanEl= document.createElement("span"); 
+const buttonEl=document.createElement("button");
+
+spanEl.textContent = element;
+buttonEl.textContent = "Delete Task";
+buttonEl.className = "btn";
+buttonEl.addEventListener("click", function () {
+
+removeTodo(index);
+console.log(todos);
+});
+
+liel.insertAdjacentElement("afterbegin",spanEl); 
+liel.insertAdjacentElement("beforeend",buttonEl);
+
+tasklist.insertAdjacentElement("afterbegin", liel);
+});
+}
+function removeTodo (index) {
+todos.splice (index, 1);
+sessionStorage.setItem("todos", JSON.stringify(todos));
+updateTaskList()
+}
